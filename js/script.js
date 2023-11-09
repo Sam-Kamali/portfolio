e63676ca8f9fb6d9c28d529dc6b6c09ac02339c6
sam kamali <kama0104@algonquinlive.com>
2023-11-09 13:20:33 -0500

    photography page complete

--- js/script.js
+++ js/script.js
@@ -183,27 +183,6 @@ downloadResumeButton.addEventListener("click", function (event) {
 // $("#next").click(function () {
 //   moveToSelected("next");
 // });
-const prevButton = document.getElementById("prev");
-const nextButton = document.getElementById("next");
-const carousel = document.querySelector(".carousel");
-const images = document.querySelectorAll(".carousel img");
 
-let currentIndex = 0;
-
-nextButton.addEventListener("click", () => {
-  currentIndex = (currentIndex + 1) % images.length;
-  updateCarousel();
-});
-
-prevButton.addEventListener("click", () => {
-  currentIndex = (currentIndex - 1 + images.length) % images.length;
-  updateCarousel();
-});
-
-function updateCarousel() {
-  const offset = -currentIndex * 300; // Adjust the width of the images
-
-  carousel.style.transform = `translateX(${offset}px)`;
-}
-
-updateCarousel();
+///////////////////////////////////////////////////////////
+// PHOTOGRAPHY
