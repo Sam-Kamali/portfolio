cab34fbc6e5da7eb8a13ef1d45773aaf1f3329c5
sam kamali <kama0104@algonquinlive.com>
2023-11-16 13:04:47 -0500

    hopefully bug fixed

--- js/script.js
+++ js/script.js
@@ -26,30 +26,30 @@ btnNavEl.addEventListener("click", function () {
 });
 
 // Smooth scrolling animation
-// const allLinks = document.querySelectorAll("a");
+const allLinks = document.querySelectorAll("a");
 
-// allLinks.forEach(function (link) {
-//   link.addEventListener("click", function (e) {
-//     const href = link.getAttribute("href");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    const href = link.getAttribute("href");
 
-//     if (href === "#") {
-//       e.preventDefault();
-//       window.scrollTo({
-//         top: 0,
-//         behavior: "smooth",
-//       });
-//     }
+    if (href === "#") {
+      e.preventDefault();
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
 
-//     if (href.startsWith("#")) {
-//       e.preventDefault();
-//       const sectionEl = document.querySelector(href);
-//       if (sectionEl) {
-//         sectionEl.scrollIntoView({ behavior: "smooth" });
-//         headerEl.classList.remove("nav-open"); // Close mobile navigation
-//       }
-//     }
-//   });
-// });
+    if (href.startsWith("#")) {
+      e.preventDefault();
+      const sectionEl = document.querySelector(href);
+      if (sectionEl) {
+        sectionEl.scrollIntoView({ behavior: "smooth" });
+        headerEl.classList.remove("nav-open"); // Close mobile navigation
+      }
+    }
+  });
+});
 
 // Sticky navigation
 const sectionHeroEl = document.querySelector(".section-hero");
@@ -77,100 +77,99 @@ if (sectionHeroEl) {
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
-// function checkFlexGap() {
-//   var flex = document.createElement("div");
-//   flex.style.display = "flex";
-//   flex.style.flexDirection = "column";
-//   flex.style.rowGap = "1px";
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
 
-//   flex.appendChild(document.createElement("div"));
-//   flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
 
-//   document.body.appendChild(flex);
-//   var isSupported = flex.scrollHeight === 1;
-//   flex.parentNode.removeChild(flex);
-//   console.log(isSupported);
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
 
-//   if (!isSupported) document.body.classList.add("no-flexbox-gap");
-// }
-// checkFlexGap();
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
 
 ///////////////////////////////////////////////////////////
 // DOWNLOADING RESUME PDF
 
-// const downloadResumeButton = document.getElementById("downloadResume");
+const downloadResumeButton = document.getElementById("downloadResume");
 
-// downloadResumeButton.addEventListener("click", function (event) {
-//   event.preventDefault(); 
+downloadResumeButton.addEventListener("click", function (event) {
+  event.preventDefault();
 
-//   const pdfUrl = downloadResumeButton.getAttribute("href");
-//   const pdfFileName = "Sam_Kamali_Resume.pdf";
+  const pdfUrl = downloadResumeButton.getAttribute("href");
+  const pdfFileName = "Sam_Kamali_Resume.pdf";
 
+  const a = document.createElement("a");
+  a.style.display = "none";
+  a.href = pdfUrl;
+  a.download = pdfFileName;
 
-//   const a = document.createElement("a");
-//   a.style.display = "none";
-//   a.href = pdfUrl;
-//   a.download = pdfFileName;
-  
-//   document.body.appendChild(a);
+  document.body.appendChild(a);
 
-//   a.click();
-//   document.body.removeChild(a);
-// });
+  a.click();
+  document.body.removeChild(a);
+});
 
 ///////////////////////////////////////////////////////////
 // CAROUSEL
-function moveToSelected(element) {
-  if (element == "next") {
-    var selected = $(".selected").next();
-  } else if (element == "prev") {
-    var selected = $(".selected").prev();
-  } else {
-    var selected = element;
-  }
+// function moveToSelected(element) {
+//   if (element == "next") {
+//     var selected = $(".selected").next();
+//   } else if (element == "prev") {
+//     var selected = $(".selected").prev();
+//   } else {
+//     var selected = element;
+//   }
 
-  var next = $(selected).next();
-  var prev = $(selected).prev();
-  var prevSecond = $(prev).prev();
-  var nextSecond = $(next).next();
+//   var next = $(selected).next();
+//   var prev = $(selected).prev();
+//   var prevSecond = $(prev).prev();
+//   var nextSecond = $(next).next();
 
-  $(selected).removeClass().addClass("selected");
+//   $(selected).removeClass().addClass("selected");
 
-  $(prev).removeClass().addClass("prev");
-  $(next).removeClass().addClass("next");
+//   $(prev).removeClass().addClass("prev");
+//   $(next).removeClass().addClass("next");
 
-  $(nextSecond).removeClass().addClass("nextRightSecond");
-  $(prevSecond).removeClass().addClass("prevLeftSecond");
+//   $(nextSecond).removeClass().addClass("nextRightSecond");
+//   $(prevSecond).removeClass().addClass("prevLeftSecond");
 
-  $(nextSecond).nextAll().removeClass().addClass("hideRight");
-  $(prevSecond).prevAll().removeClass().addClass("hideLeft");
-}
+//   $(nextSecond).nextAll().removeClass().addClass("hideRight");
+//   $(prevSecond).prevAll().removeClass().addClass("hideLeft");
+// }
 
-// Eventos teclado
-$(document).keydown(function (e) {
-  switch (e.which) {
-    case 37: // left
-      moveToSelected("prev");
-      break;
+// // Eventos teclado
+// $(document).keydown(function (e) {
+//   switch (e.which) {
+//     case 37: // left
+//       moveToSelected("prev");
+//       break;
 
-    case 39: // right
-      moveToSelected("next");
-      break;
+//     case 39: // right
+//       moveToSelected("next");
+//       break;
 
-    default:
-      return;
-  }
-  e.preventDefault();
-});
+//     default:
+//       return;
+//   }
+//   e.preventDefault();
+// });
 
-$("#carousel div").click(function () {
-  moveToSelected($(this));
-});
+// $("#carousel div").click(function () {
+//   moveToSelected($(this));
+// });
 
-$("#prev").click(function () {
-  moveToSelected("prev");
-});
+// $("#prev").click(function () {
+//   moveToSelected("prev");
+// });
 
-$("#next").click(function () {
-  moveToSelected("next");
-});
+// $("#next").click(function () {
+//   moveToSelected("next");
+// });
