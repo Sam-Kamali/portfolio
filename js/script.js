b8cdd32ca3d406f6afb807d01588c4db9ad50348
sam kamali <kama0104@algonquinlive.com>
2023-11-15 23:43:34 -0500

    updates

--- js/script.js
+++ js/script.js
@@ -26,30 +26,30 @@ btnNavEl.addEventListener("click", function () {
 });
 
 // Smooth scrolling animation
-const allLinks = document.querySelectorAll("a");
+// const allLinks = document.querySelectorAll("a");
 
-allLinks.forEach(function (link) {
-  link.addEventListener("click", function (e) {
-    const href = link.getAttribute("href");
+// allLinks.forEach(function (link) {
+//   link.addEventListener("click", function (e) {
+//     const href = link.getAttribute("href");
 
-    if (href === "#") {
-      e.preventDefault();
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
-    }
+//     if (href === "#") {
+//       e.preventDefault();
+//       window.scrollTo({
+//         top: 0,
+//         behavior: "smooth",
+//       });
+//     }
 
-    if (href.startsWith("#")) {
-      e.preventDefault();
-      const sectionEl = document.querySelector(href);
-      if (sectionEl) {
-        sectionEl.scrollIntoView({ behavior: "smooth" });
-        headerEl.classList.remove("nav-open"); // Close mobile navigation
-      }
-    }
-  });
-});
+//     if (href.startsWith("#")) {
+//       e.preventDefault();
+//       const sectionEl = document.querySelector(href);
+//       if (sectionEl) {
+//         sectionEl.scrollIntoView({ behavior: "smooth" });
+//         headerEl.classList.remove("nav-open"); // Close mobile navigation
+//       }
+//     }
+//   });
+// });
 
 // Sticky navigation
 const sectionHeroEl = document.querySelector(".section-hero");
@@ -77,59 +77,53 @@ if (sectionHeroEl) {
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
-function checkFlexGap() {
-  var flex = document.createElement("div");
-  flex.style.display = "flex";
-  flex.style.flexDirection = "column";
-  flex.style.rowGap = "1px";
+// function checkFlexGap() {
+//   var flex = document.createElement("div");
+//   flex.style.display = "flex";
+//   flex.style.flexDirection = "column";
+//   flex.style.rowGap = "1px";
 
-  flex.appendChild(document.createElement("div"));
-  flex.appendChild(document.createElement("div"));
+//   flex.appendChild(document.createElement("div"));
+//   flex.appendChild(document.createElement("div"));
 
-  document.body.appendChild(flex);
-  var isSupported = flex.scrollHeight === 1;
-  flex.parentNode.removeChild(flex);
-  console.log(isSupported);
+//   document.body.appendChild(flex);
+//   var isSupported = flex.scrollHeight === 1;
+//   flex.parentNode.removeChild(flex);
+//   console.log(isSupported);
 
-  if (!isSupported) document.body.classList.add("no-flexbox-gap");
-}
-checkFlexGap();
+//   if (!isSupported) document.body.classList.add("no-flexbox-gap");
+// }
+// checkFlexGap();
 
 ///////////////////////////////////////////////////////////
 // DOWNLOADING RESUME PDF
 
-// Get a reference to the "Download Resume" button.
-const downloadResumeButton = document.getElementById("downloadResume");
+// const downloadResumeButton = document.getElementById("downloadResume");
 
-// Add a click event listener to trigger the download.
-downloadResumeButton.addEventListener("click", function (event) {
-  event.preventDefault(); // Prevent the link from navigating to the PDF file.
+// downloadResumeButton.addEventListener("click", function (event) {
+//   event.preventDefault(); 
 
-  const pdfUrl = downloadResumeButton.getAttribute("href");
-  const pdfFileName = "Sam_Kamali_Resume.pdf";
+//   const pdfUrl = downloadResumeButton.getAttribute("href");
+//   const pdfFileName = "Sam_Kamali_Resume.pdf";
 
-  // Create an anchor element to trigger the download.
-  const a = document.createElement("a");
-  a.style.display = "none";
-  a.href = pdfUrl;
-  a.download = pdfFileName;
 
-  // Add the anchor element to the document.
-  document.body.appendChild(a);
+//   const a = document.createElement("a");
+//   a.style.display = "none";
+//   a.href = pdfUrl;
+//   a.download = pdfFileName;
+  
+//   document.body.appendChild(a);
 
-  // Trigger the click event on the anchor element to start the download.
-  a.click();
-
-  // Clean up by removing the anchor element.
-  document.body.removeChild(a);
-});
+//   a.click();
+//   document.body.removeChild(a);
+// });
 
 ///////////////////////////////////////////////////////////
 // CAROUSEL
 function moveToSelected(element) {
-  if (element === "next") {
+  if (element == "next") {
     var selected = $(".selected").next();
-  } else if (element === "prev") {
+  } else if (element == "prev") {
     var selected = $(".selected").prev();
   } else {
     var selected = element;
@@ -140,37 +134,35 @@ function moveToSelected(element) {
   var prevSecond = $(prev).prev();
   var nextSecond = $(next).next();
 
-  $(".selected").removeClass().addClass("hideLeft");
+  $(selected).removeClass().addClass("selected");
+
   $(prev).removeClass().addClass("prev");
   $(next).removeClass().addClass("next");
+
   $(nextSecond).removeClass().addClass("nextRightSecond");
   $(prevSecond).removeClass().addClass("prevLeftSecond");
 
-  setTimeout(function () {
-    $(".hideLeft").removeClass().addClass("hideRight");
-  }, 300);
-  setTimeout(function () {
-    $(".hideRight").removeClass().addClass("hide");
-    selected.removeClass().addClass("selected");
-  }, 500);
+  $(nextSecond).nextAll().removeClass().addClass("hideRight");
+  $(prevSecond).prevAll().removeClass().addClass("hideLeft");
 }
 
-// Keyboard Events
+// Eventos teclado
 $(document).keydown(function (e) {
   switch (e.which) {
     case 37: // left
       moveToSelected("prev");
       break;
+
     case 39: // right
       moveToSelected("next");
       break;
+
     default:
       return;
   }
   e.preventDefault();
 });
 
-// Click Events
 $("#carousel div").click(function () {
   moveToSelected($(this));
 });
